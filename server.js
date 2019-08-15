83c27ffb9d0d4f621f13ec4e713172602f79fec1
Andy Chen <andycandomore@gmail.com>
2019-08-14 22:10:24 -0700

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+const NodeServer = require('@amazingandyyy/node-server')
+const bodyParser = require('@amazingandyyy/body-parser')
+const logger = require('@amazingandyyy/server-logger')
+const app = new NodeServer()
+
+app.use(logger('dev'))
+app.use(bodyParser)
+
+app.on('get', '/', (req, res) => {
+  res.send(200)
+})
+
+app.on('post', '/ping', (req, res) => {
+  res.send(req.body)
+})
+
+
+app.start({ port: '4000' })
